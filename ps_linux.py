42a7a4c17fddc50890ade5b4a5bf54f24ee279e0
Justin Timperio <justintimperio@gmail.com>
2019-11-10 13:23:07 -0500

    renamed:    gztar_bash.py -> other/gztar_bash.py         renamed:    gztar.py -> ps_gztar.py         renamed:    global_defuns.py -> ps_linux.py         new file:   ps_networking.py

--- ps_linux.py
+++ ps_linux.py
@@ -1,71 +1,5 @@
 #! /usr/bin/python
-###### Useful Defs for Python3 Projects
-#### Version - 1.5
-
-######
-## Basic Functions
-######
-import os, sys, subprocess
-
-def date_to_today(year, month, day, set_name):
-    import datetime
-    start_date = datetime.date(year, month, day)
-    end_date = datetime.date.today() - datetime.timedelta(days=1)
-    date_delta = abs((start_date - datetime.date.today()).days)
-    list_name = {str(end_date - datetime.timedelta(days=x)) for x in range(0, date_delta)}
-    return set_name
-
-def yn_frame(prompt, y_defun, n_defun):
-    yn = input(prompt + '? (y/n):')
-    if yn.lower() in ['y','yes']: 
-        y_defun
-    elif yn.lower() in ['no','n']:
-        n_defun
-    else: sys.exit('No usable argument given!') 
-
-def export_list(file_name, list_name):
-    if os.path.exists(file_name):
-        os.remove(file_name)
-    with open(file_name, 'w') as f:
-        for i in list_name:
-            f.write("%s\n" % i) 
-
-def read_list(file_name):
-    list_name = list(open(file_name).read().splitlines()) 
-    return list_name
-
-######
-### Network and Web Fuctions
-######
-
-def dl_url(url, file_name):
-    import requests
-    r = requests.get(url, allow_redirects=True)
-    open(file_name, 'wb').write(r.content)
-
-def is_url_downloadable(url):
-    import requests
-    h = requests.head(url, allow_redirects=True)
-    header = h.headers
-    content_type = header.get('content-type')
-    if 'text' in content_type.lower():
-        return False
-    if 'html' in content_type.lower():
-        return False
-    return True
-
-def get_local_ip():
-    import socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
+#### Linux Commands - 1.0
 
 ######
 ### File System Commands and Short-Cuts
@@ -132,4 +66,3 @@ def pip_install(packages):
 
 def aurman_install(packages):
     os.system("aurman -S --needed " + packages)
-
---
+++ ps_networking.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+#### Network Commands - v1.0
+
+######
+### Network and Web Fuctions
+######
+
+def dl_url(url, file_name):
+    import requests
+    r = requests.get(url, allow_redirects=True)
+    open(file_name, 'wb').write(r.content)
+
+def is_url_downloadable(url):
+    import requests
+    h = requests.head(url, allow_redirects=True)
+    header = h.headers
+    content_type = header.get('content-type')
+    if 'text' in content_type.lower():
+        return False
+    if 'html' in content_type.lower():
+        return False
+    return True
+
+def get_local_ip():
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
