ca978c07e1fcb5107276ea1672a229998bafc8c1
Justin Timperio <justintimperio@gmail.com>
2020-07-28 22:59:04 -0400

    Minor updates to archive fuctions. Checksum func now returns tuple instead of str

--- ps_archives.py
+++ ps_archives.py
@@ -5,6 +5,7 @@ import gzip
 import tarfile
 import shutil
 import hashlib
+import multiprocessing
 from .ps_linux import Search_FS
 
 
@@ -14,8 +15,8 @@ def GZ_C(path, rm=False):
             shutil.copyfileobj(f, gz)
     if rm is True:
         os.remove(path)
-    if rm is False:
-        return
+    elif rm is False:
+        pass
 
 
 def GZ_D(path, rm=False):
@@ -24,8 +25,8 @@ def GZ_D(path, rm=False):
             shutil.copyfileobj(gz, f)
     if rm is True:
         os.remove(path)
-    if rm is False:
-        return
+    elif rm is False:
+        pass
 
 
 def Tar_Dir(path, rm=False):
@@ -34,8 +35,8 @@ def Tar_Dir(path, rm=False):
             tar.add(f, f[len(path):])
     if rm is True:
         shutil.rmtree(path)
-    if rm is False:
-        return
+    elif rm is False:
+        pass
 
 
 def Untar_Dir(path, rm=False):
@@ -43,22 +44,26 @@ def Untar_Dir(path, rm=False):
         tar.extractall(path[:-4])
     if rm is True:
         os.remove(path)
-    if rm is False:
-        return
+    elif rm is False:
+        pass
 
 
 def Checksum_File(file_path):
-    '''Checksums a file using hashlib.md5(). Reads File in 250MB Chunks.'''
+    '''
+    Checksums a file using hashlib.md5(). Reads the file in 250MB chunks.
+    Checksum is slow as fuck on anything larger than 5GB so they are ignored.
+    Hopefully I'll fix this later by using raw linux commands
+    '''
     if not os.path.exists(file_path):
-        return str(file_path + ' : FILE MISSING!')
-    size = os.path.getsize(file_path)
-    if size == 0:
-        return str(file_path + ' : 0')
+        return (file_path, 'FILE MISSING!')
+    else:
+        size = os.path.getsize(file_path)
 
-    # Checksum is slow as fuck so i'm ignoring anything larger than 5GB
-    # Hopefully I'll fix this later by using raw linux commands
+    if size == 0:
+        return (file_path, '0')
+
     elif size > 5368709120:
-        return str(file_path + ' : TOO LARGE!')
+        return (file_path, 'TOO LARGE!')
 
     else:
         with open(file_path, 'rb') as fh:
@@ -68,32 +73,39 @@ def Checksum_File(file_path):
                 if not data:
                     break
                 m.update(data)
-            return str(file_path + ' : ' + str(m.hexdigest()))
+            return (file_path, str(m.hexdigest()))
 
 
 def GZTar_C(dir_path, queue_depth=round(os.cpu_count()*.75), rmbool=True):
-    '''Compress files individually in a dir using mp.pool, then tar files.'''
-    import multiprocessing
+    '''
+    Compress files individually in a dir using mp.pool, then tar files.
+    This compresses files BEFORE adding them to the tar.
+    '''
     import tqdm
+
     files = Search_FS(dir_path)
     with multiprocessing.Pool(queue_depth) as pool:
-        list(tqdm.tqdm(pool.imap(GZ_C, files),
-                       total=len(files), desc='Compressing Files'))
+        list(tqdm.tqdm(pool.imap(GZ_C, files), total=len(files), desc='Compressing Files'))
+
     print('Adding Compressed Files to TAR....')
     Tar_Dir(dir_path)
+
     if rmbool is True:
         shutil.rmtree(dir_path)
 
 
 def GZTar_D(tar_path, queue_depth=round(os.cpu_count()*.75), rmbool=True):
-    '''Unpack a tar then individually decompress each file using mp.pool().'''
-    import multiprocessing
+    '''
+    Unpack a tar then individually decompress each file using mp.pool().
+    This should be used in combo with GZTar_C().
+    '''
     import tqdm
+
     print('Extracting Files From TAR....')
     Untar_Dir(tar_path)
     if rmbool is True:
         os.remove(tar_path)
+
     files = Search_FS(tar_path[:-4])
     with multiprocessing.Pool(queue_depth) as pool:
-        list(tqdm.tqdm(pool.imap(GZ_D, files),
-                       total=len(files), desc='Decompressing Files'))
+        list(tqdm.tqdm(pool.imap(GZ_D, files), total=len(files), desc='Decompressing Files'))
